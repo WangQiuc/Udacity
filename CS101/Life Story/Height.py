eb924a1180a3006788c05d2b61164d2f07a8961b
王秋成 <32641402+WangQiuc@users.noreply.github.com>
2017-12-20 14:43:44 +0800

    Delete Height.py

--- CS101/Life Story/Height.py
+++
@@ -1,9 +0,0 @@
-def next_height(x, i):
-    if i == 0:
-        print(x)
-        return x
-    else:
-        print(0.516*x + 0.8567)
-        return next_height(0.516*x + 0.8567, i - 1)
-
-next_height(2.2, 20)
